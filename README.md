365edcee99f0ecaaf876cbffcf182c2eddcfd87a
iUseMintBTW <transformers2004@proton.me>
2023-12-14 21:28:16 -0300

    agora sim

--- README.md
+++
@@ -1,1 +0,0 @@
-# arkanus-profile-card-app
---
+++ code.js
@@ -0,0 +1,35 @@
+const tag = "brasino"
+let profile = document.getElementById("profile")
+let bubble = document.getElementById("bubble")
+let aggressivity = 0;
+let isTimeoutSet = false;
+
+function clipboard ()
+{
+    navigator.clipboard.writeText(tag)
+    bubble.style.opacity = 1
+    setTimeout(() => {
+        bubble.style.opacity = 0;
+    }, 1000);
+}
+
+function swapProfile ()
+{
+    if (aggressivity + 1 > 3)
+        aggressivity = 0;
+    else
+        aggressivity++;
+
+    profile.src = `profile${aggressivity}.webp`;
+
+    if (!isTimeoutSet)
+    {
+        setTimeout(() => {
+            profile.src = `profile0.webp`;
+            aggressivity = 0;
+            isTimeoutSet = false;
+        }, 5000)
+
+        isTimeoutSet = true;
+    }
+}
